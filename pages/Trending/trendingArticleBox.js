8ff0c08e169db4e582129a2ef202b35e9bf494d9
shiran-dinushka <shirandinushka9@gmail.com>
2023-06-16 08:53:50 +0530

    add trending article

--- pages/Trending/trendingArticleBox.js
+++ pages/Trending/trendingArticleBox.js
@@ -1,25 +1,62 @@
-import * as React from 'react';
+import React,{ useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import ArticleCard from '../../components/articleCard';
+import Link from 'next/link';
 
-function TrendingArticleBox() {
- 
-  return (
-    <div style={{marginTop:'20px', width:'100%'}}>
-        <Grid sx={{ flexGrow: 1 }}>
-            <Grid item xs={12}>
-                <Grid container justifyContent="center" spacing={3}>
-                {[0, 1, 2,3,4,5,6,7].map((value) => (
-                    <Grid key={value} item>
-                        <ArticleCard />
+
+function TrendingArticleBox(props) {
+        const url = `/api/searchApi?keyword=${props.keyword}`;
+        const method = 'GET';
+        const [data, setData] = useState([]);
+        const option = {
+            method: method,
+            
+        }
+        useEffect(() => {
+            const fetchData = async () => {
+            console.log("response1");
+            try{
+                    const response  = await fetch(url, option);
+                    const finalData = await response.json();
+                    setData(finalData);
+                    console.log("response");
+            }catch(error){
+
+                    console.error(error);
+                    console.log("responseee");
+            }
+           
+          }
+          fetchData()
+          
+        }, [props.keyword]) 
+
+       console.log(data);
+
+  
+
+        return (
+            <div style={{marginTop:'20px', width:'100%'}}>
+                <Grid sx={{ flexGrow: 1 }}>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={3}>
+                        {data.map((item) => (
+                            <Grid key={item.articleId} item>
+                                 <Link href={`/article?id=${item.articleId}`} passHref>
+                                    <ArticleCard name={item.title}  />
+                                </Link>
+                               
+                            
+                            </Grid>
+                        ))}
+                        </Grid>
                     </Grid>
-                ))}
-                </Grid>
-            </Grid>
-        </Grid>
-    </div>
-    
-  );
+                        </Grid>
+                
+            </div>
+            
+        );
 }
+
 
 export default TrendingArticleBox;
--- pages/api/auth/reset-password.js
+++ pages/api/auth/reset-password.js
@@ -2,6 +2,24 @@ const handler = async(req, res) => {
 
     console.log(req.body);
     // Set up CORS headers with options
+
+    const method = 'GET';
+    const url = `https://6w0yznyace.execute-api.eu-north-1.amazonaws.com/prod/products`;
+    const option = {
+        method: method,
+    
+    }
+
+// Set up CORS headers with options
+  try{
+        const response = await fetch(url,option);
+        const finalData1 = await response.json();
+
+
+      } catch (error) {
+         console.error(error)
+         return res.status(error.status || 500).end(error.message);
+    }
     try{
         const response = await fetch(`https://6w0yznyace.execute-api.eu-north-1.amazonaws.com/prod/reset`, {
             method: 'POST',
@@ -10,12 +28,18 @@ const handler = async(req, res) => {
               'Content-Type': 'application/json'
             }
           });
+
     
             return res.status(200).json(response);
           } catch (error) {
              console.error(error)
              return res.status(error.status || 500).end(error.message);
         }
+
+        
+       
+
+      
 }
   
 export default handler;
