1500e6ed08835bbdb122b2fede4c3ff9adbd26e3
chamodya agrasari <chamodyaagra2000@gmail.com>
2024-02-11 15:17:39 +0530

    Add dialog box before editing and deleting a topic domain

---
+++ components/DeleteConfirmationDialog.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
+function DeleteConfirmationDialog({ open, onClose, onConfirm }) {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Confirmation</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to delete this topic domain ?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={onConfirm} autoFocus>
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default DeleteConfirmationDialog;
