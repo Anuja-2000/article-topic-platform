b58a944d8f20f4a6216bb48768b73fe518905ef5
shiran-dinushka <shirandinushka9@gmail.com>
2024-02-19 23:54:40 +0530

    remove few searching parameters

--- components/article/articleCard.js
+++ components/article/articleCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, CardActions, Button, CardMedia, Avatar, Box, Chip } from '@mui/material';
 
-const ArticleCard = ({ title, date, writer, writerImage, articleImage, tags }) => {
+const ArticleCard = ({ title, date, writer, profilePic,image, tags }) => {
   return (
     <Card style={{ border: '1px solid #ddd', borderRadius: '8px', width: 300, height: '100%', backgroundColor: '#f5f5f5', margin: '10px' }}>
       <CardContent>
@@ -9,27 +9,27 @@ const ArticleCard = ({ title, date, writer, writerImage, articleImage, tags }) =
           <Box display="flex" alignItems="center">
             <Avatar
               alt={writer}
-              src={writerImage}
-              style={{ width: '40px', height: '40px', marginRight: '12px' }}
+              src={profilePic}
+              style={{ width: '30px', height: '30px', marginRight: '8px' }}
             />
-            <Typography color="text.primary" variant="subtitle1" style={{ fontWeight: 600 }}>
+            <Typography color="text.primary" variant="subtitle1" style={{ fontWeight: 600, fontSize: 12 }}>
               {writer}
             </Typography>
           </Box>
-          <Typography color="text.secondary" variant="body2">
+          <Typography color="text.secondary" variant="body2" style={{ fontSize: 10 }}>
             {date}
           </Typography>
         </Box>
-        <Typography variant="h6" component="div" style={{ marginTop: '12px', fontWeight: 700 }}>
+        <Typography variant="h6" component="div" style={{ marginTop: '12px', fontWeight: 700,fontSize: 20 }}>
           {title}
         </Typography>
         <Box marginTop="10px">
-          {tags.map((tag, index) => (
+          {/*tags.map((tag, index) => (
             <Chip key={index} label={tag} size="small" style={{ marginRight: '8px', backgroundColor: '#f6e58d'}} />
-          ))}
+          ))*/}
         </Box>
       </CardContent>
-      <CardMedia component="img" height="140" src={articleImage} alt={title} style={{marginBottom: '15px',borderRadius: '4px' }} />
+      <CardMedia component="img" height="140" src={image} alt={title} style={{marginBottom: '15px',borderRadius: '4px' }} />
     </Card>
   );
 };
