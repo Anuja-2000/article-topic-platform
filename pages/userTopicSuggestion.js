74cf3f0a62929bb774efa0e10ac8223ed2882446
chamodya agrasari <chamodyaagra2000@gmail.com>
2024-02-19 18:12:44 +0530

    Change appearance of page

--- pages/userTopicSuggestion.js
+++ pages/userTopicSuggestion.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Navbar from '../components/Navbar';
-import SearchIcon from '@mui/icons-material/Search';
+import Image from 'next/image';
 import searchTopicImage from '../public/asset/searchTopicImage.jpg';
 
 const UserTopicSuggestion = () => {
@@ -16,8 +16,8 @@ const UserTopicSuggestion = () => {
   const [selectedTopicDomain, setSelectedTopicDomain] = useState('');
   const [keywords, setKeywords] = useState([]);
   const [selectedKeywords, setSelectedKeywords] = useState([]);
-  const [searchResults, setSearchResults] = useState([]); // State to store search results
-  const [searchClicked, setSearchClicked] = useState(false); // State to track if search button is clicked
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchClicked, setSearchClicked] = useState(false);
 
   useEffect(() => {
     const fetchTopicDomains = async () => {
@@ -54,28 +54,23 @@ const UserTopicSuggestion = () => {
     });
   };
 
-  
   const handleSearch = async () => {
-    // Implement search functionality based on selected topic domain and keywords
     try {
       const response = await axios.get(`http://localhost:3001/api/topics/get/${selectedTopicDomain}/${selectedKeywords}`);
       setSearchResults(response.data);
-      console.log(response.data);
-      setSearchClicked(true); // Set search button clicked to true
+      setSearchClicked(true);
     } catch (error) {
       console.error('Error fetching search results:', error);
     }
   };
-  
 
   return (
     <Navbar>
       <div className="App" style={{ marginTop: '60px' }}>
         <Grid container spacing={3}>
-          {/* Filters Section */}
           <Grid item xs={12} sm={6}>
             <div style={{ marginRight: '20px' }}>
-              <Card style={{ backgroundColor: '#1E1E3C', color: 'white' }}>
+              <Card style={{ backgroundColor: '#1E1E3C', color: 'white', borderRadius: '20px' }}>
                 <CardContent>
                   <Typography variant="h6" gutterBottom>Topic Domains</Typography>
                   {topicDomains.map((domain) => (
@@ -88,7 +83,7 @@ const UserTopicSuggestion = () => {
                   ))}
                 </CardContent>
               </Card>
-              <Card style={{ backgroundColor: '#1E1E3C', color: 'white', marginTop: '20px' }}>
+              <Card style={{ backgroundColor: '#1E1E3C', color: 'white', marginTop: '20px', borderRadius: '20px' }}>
                 <CardContent>
                   <Typography variant="h6" gutterBottom>Keywords</Typography>
                   {keywords.map((keyword) => (
@@ -101,34 +96,41 @@ const UserTopicSuggestion = () => {
                   ))}
                 </CardContent>
               </Card>
-              <Card style={{ backgroundColor: '#1E1E3C', color: 'white', marginTop: '20px' }}>
+              <Card style={{ backgroundColor: '#1E1E3C', color: 'white', marginTop: '20px', borderRadius: '20px' }}>
                 <CardContent>
                   <Typography variant="h6" gutterBottom>Trending Keywords</Typography>
                   {/* Add trending keywords here */}
                 </CardContent>
               </Card>
               <Button variant="contained" color="primary" onClick={handleSearch} disabled={!selectedTopicDomain || selectedKeywords.length === 0} style={{ marginTop: '20px' }}>Search</Button>
-
             </div>
           </Grid>
-          {/* Right Side Section */}
           <Grid item xs={12} sm={6}>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               {!searchClicked ? (
-                <>
-                  <Typography variant="h6" style={{ marginBottom: '10px' }}>Lets start</Typography>
-                  <img src={searchTopicImage} alt="Placeholder" style={{ maxWidth: '100%' }} />
-                </>
-              ) : (
-                <Card style={{ backgroundColor: '#E3F2FD', padding: '20px', width: '100%' }}>
+                <Card style={{ backgroundColor: '#1E1E3C', padding: '20px', width: '100%', borderRadius: '20px' }}>
                   <CardContent>
-                    <Typography variant="h6" gutterBottom>Search Results</Typography>
+                    <Typography variant="h4" style={{ marginBottom: '10px', textAlign: 'center', fontWeight: 'bold', fontStyle: 'italic', color: '#1e90ff' }}>Lets Start</Typography>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                      <Image src={searchTopicImage} alt="Placeholder" width={500} height={300} />
+                    </div>
+                  </CardContent>
+                </Card>
+              ) : (
+                <Card style={{ backgroundColor: '#E3F2FD', padding: '20px', width: '100%', borderRadius: '20px' }}>
+                  <CardContent>
+                    <Typography variant="h4" gutterBottom style={{ marginBottom: '10px', textAlign: 'center', fontWeight: 'bold', fontStyle: 'italic', color: '#1e90ff' }}> Search Results</Typography>
                     {searchResults.map((result) => (
-                      <div key={result._id}>
-                        <Typography variant="h6">{result.topicName}</Typography>
-                        <Typography variant="body1">{result.description}</Typography>
+                      <div key={result.topicId}>
+                        <ul>
+                          <li key={result.topicId}>
+                            <Typography variant="h6" style={{ marginBottom: '8px' }}>{result.topicName}</Typography>
+                            <Typography variant="body1">{result.description}</Typography>
+                          </li>
+                        </ul>
                       </div>
                     ))}
+
                   </CardContent>
                 </Card>
               )}
