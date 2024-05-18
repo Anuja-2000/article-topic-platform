79ca89243e7af8957320f68e23ad38cfe0672d45
Anuja-2000 <anujawij@gmail.com>
2024-05-18 18:00:22 +0530

    changed base url to hosted backend

--- enums/url.js
+++ enums/url.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001/api/';
+const BASE_URL = 'https://article-writing-backend.onrender.com/api/';
 const urls = {
     BASE_URL_USER : `${BASE_URL}user/`,
     BASE_URL_FILE : `${BASE_URL}file/`,
