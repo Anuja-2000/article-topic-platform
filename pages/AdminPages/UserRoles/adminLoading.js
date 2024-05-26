b75ddc38db9cd9670e2b8f509aaf2eb4ef77b809
Anuja99x <anujaw@99x.io>
2024-05-26 22:53:18 +0530

    added skeletons to userroles

--- pages/AdminPages/UserRoles/adminLoading.js
+++ pages/AdminPages/UserRoles/adminLoading.js
@@ -1,25 +1,22 @@
 import { TableCell, Skeleton, TableRow } from "@mui/material";
 
 const rows = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
     // note: we are adding a key prop here to allow react to uniquely identify each
     // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
     rows.push(<TableRow>
       <TableCell>
-        <Skeleton variant="rectangular" width={100} height={20} />
+        <Skeleton variant="rectangular" width={150} height={20} />
       </TableCell>
       <TableCell>
-        <Skeleton variant="rectangular" width={100} height={20} />
+        <Skeleton variant="rectangular" width={200} height={20} />
       </TableCell>
       <TableCell>
-        <Skeleton variant="rectangular" width={100} height={20} />
-      </TableCell>
-      <TableCell>
-        <center><Skeleton variant="rounded" width={50} height={25} /></center>
+        <Skeleton variant="circular" width={60} height={30} />
       </TableCell>
       </TableRow>);
 }
-export default function Loading() {
+export default function AdminLoading() {
   return (
     <>
     {rows}
